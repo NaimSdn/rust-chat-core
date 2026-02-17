944e9182e01d68089ddeb5cc89d4e58dd8bc739e
NaimSdn <saadoune.naim.pro@gmail.com>
2026-02-18 00:20:09 +0100

    Updating | Client is now able to send message to server, and server is capable to print what client sent

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,19 +1,20 @@
+use std::io::{stdin, Write};
 use std::net::TcpStream;
 
 fn main() {
-
-    /*
-        TODO - CONNECT TO THE SERVER
-        TODO - https://doc.rust-lang.org/std/net/struct.TcpStream.html : CONNECT
-     */
-
-    let server_ip_addr = String::from("127.0.0.1");
-    let server_port = String::from("8080");
-
-    if let Ok(_request) = TcpStream::connect(format!("{server_ip_addr}:{server_port}")) {
-        println!("Connected to the server !");
+    let server_ip_addr: String = String::from("127.0.0.1");
+    let server_port: String = String::from("8080");
+    if let Ok(mut request) = TcpStream::connect(format!("{server_ip_addr}:{server_port}")) {
+        println!("Connexion established with the server !");
+        let mut user_input: String = String::new();
+        match stdin().read_line(&mut user_input) {
+            Ok(_n) => {
+                let user_byte: &[u8] = user_input.as_bytes();
+                request.write(user_byte).expect("Error on the write method !");
+            }
+            Err(err) => {  println!("Error : {err}"); }
+        }
     } else {
         println!("Couldn't connect to the server !");
     }
-
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,22 +1,20 @@
+use std::io::Read;
 use std::net::TcpListener;
 
 fn main() {
-    /*
-        TODO - START A SERVER
-        TODO - https://doc.rust-lang.org/std/net/struct.TcpListener.html : BIND / INCOMING METHODS
-     */
-
-    let server_ip_addr = String::from("127.0.0.1");
-    let server_port = String::from("8080");
-
-    let listener = TcpListener::bind(format!("{server_ip_addr}:{server_port}")).unwrap();
-
+    let server_ip_addr: String = String::from("127.0.0.1");
+    let server_port: String = String::from("8080");
+    let listener: TcpListener = TcpListener::bind(format!("{server_ip_addr}:{server_port}")).unwrap();
     for request in listener.incoming() {
         match request {
-            Ok(_request) => {
-                println!("Connexion established !");
+            Ok( mut n) => {
+                println!("Connexion established with the client !");
+                let mut buffer: [u8; 50] = [0; 50];
+                let request_bytes_size = n.read(&mut buffer).expect("Error on the read method !");
+                let words: String = String::from_utf8(buffer[..request_bytes_size].to_vec()).unwrap();
+                println!("Client words : {words}");
             }
-            Err(err) => { println!("Connexion failed error : {err}")}
+            Err(err) => { println!("Connexion failed error : {err}") }
         }
     }
 }
